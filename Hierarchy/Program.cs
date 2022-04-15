8f29c070c19c4f4b7fc262f5efc2350b02bac839
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-04-15 10:44:36 +0300

    Add files via upload

---
+++ Hierarchy/Program.cs
@@ -0,0 +1,27 @@
+﻿using SimpleTreeNode;
+using System;
+
+namespace Hierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode<string> tree = new TreeNode<string>("Boos",
+                new TreeNode<string>("Sarah",
+                  new TreeNode<string>("Lora"),
+                  new TreeNode<string>("Viktor")),
+                new TreeNode<string>("Evan",
+                 new TreeNode<string>("Cole",
+                  new TreeNode<string>("Mary"),
+                  new TreeNode<string>("Clare")),
+                 new TreeNode<string>("Nicole",
+                  new TreeNode<string>("Alex",
+                  new TreeNode<string>("Peter")),
+                  new TreeNode<string>("Anya"))),
+                new TreeNode<string>("Mike"));
+            Console.WriteLine(tree);
+        }
+        
+    }
+}
---
+++ Hierarchy/Tree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hierarchy
+{
+    public class Tree<T>
+    {
+        public T Value { get; set; }
+
+        public Tree<T> Parent { get; set; }
+
+        public List<Tree<T>> Children { get; set; }
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.Value = value;
+            this.Children = new List<Tree<T>>();
+
+            foreach (var item in children)
+            {
+                item.Parent = this;
+                Children.Add(item);
+            }
+        }
+    }
+}
